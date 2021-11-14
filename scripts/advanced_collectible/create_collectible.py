7006059e6228373a458b45ebc80ac6952d8841f7
juliocslima <lima.julio.cs@gmail.com>
2021-11-14 11:04:31 -0300

    Set token URI function created

--- scripts/advanced_collectible/create_collectible.py
+++ scripts/advanced_collectible/create_collectible.py
@@ -6,7 +6,8 @@ from web3 import Web3
 def main():
     account = get_account()
     advanced_collectible = AdvancedCollectible[-1]
-    fund_with_link(advanced_collectible.address, Web3.toWei(0.1, "ether"))
+    fund_with_link(advanced_collectible.address,
+                   amount=Web3.toWei(0.1, "ether"))
     tx = advanced_collectible.createCollectible({"from": account})
     tx.wait(1)
     print("Collectible created!")
